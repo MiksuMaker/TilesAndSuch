d5713a19abdb3049540c70d8ba303cb76a7c101f
MiksuMaker <91052332+MiksuMaker@users.noreply.github.com>
2023-01-23 10:05:25 +0200

    Added Coordinate Drawer

---
+++ Assets/xMATH/CoordinateDrawer.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoordinateDrawer : MonoBehaviour
+{
+    #region Properties
+
+    [Header("Handle")]
+    [SerializeField] GameObject handle;
+
+
+    [Header("Coordinates")]
+    [SerializeField] float circleRadius = 1f;
+    [SerializeField] float arrowLength = 1f;
+
+    [Header("Haka Angle")]
+    [SerializeField] float hakaMultiplier = 0.5f;
+    [SerializeField] float hakaAngle = 45f;
+
+    #endregion
+
+    #region GIZMOS
+
+    private void OnDrawGizmos()
+    {
+        // Draw Circle
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(Vector3.zero, circleRadius);
+
+        // Draw Arrow Towards Handle
+        Gizmos.color = Color.black;
+        Gizmos.DrawLine(Vector3.zero, handle.transform.position);
+        Vector3 dir = (handle.transform.position - transform.position).normalized;
+        Gizmos.DrawRay(handle.transform.position, Quaternion.AngleAxis(hakaAngle, Vector3.forward) * -dir * hakaMultiplier);
+        Gizmos.DrawRay(handle.transform.position, Quaternion.AngleAxis(-hakaAngle, Vector3.forward) * -dir * hakaMultiplier);
+
+
+
+
+
+        // Draw arrows away from the Radius Circle
+        Gizmos.color = Color.green;                 
+        // UP
+        Gizmos.DrawRay(new Vector3(0f, circleRadius), Vector3.up * arrowLength);
+        Gizmos.DrawRay(new Vector3(0f, circleRadius + arrowLength), Quaternion.AngleAxis(-hakaAngle, Vector3.forward) * Vector3.down * hakaMultiplier);
+        Gizmos.DrawRay(new Vector3(0f, circleRadius + arrowLength), Quaternion.AngleAxis(hakaAngle, Vector3.forward) * Vector3.down * hakaMultiplier);
+
+        // DOWN
+        Gizmos.DrawRay(new Vector3(0f, -circleRadius), Vector3.down * arrowLength);
+        Gizmos.DrawRay(new Vector3(0f, -(circleRadius + arrowLength)), Quaternion.AngleAxis(-hakaAngle, Vector3.forward) * Vector3.up * hakaMultiplier);
+        Gizmos.DrawRay(new Vector3(0f, -(circleRadius + arrowLength)), Quaternion.AngleAxis(hakaAngle, Vector3.forward) * Vector3.up * hakaMultiplier);
+
+
+
+        Gizmos.color = Color.red;
+        // RIGHT
+        Gizmos.DrawRay(new Vector3(circleRadius, 0f), Vector3.right * arrowLength);
+        Gizmos.DrawRay(new Vector3((circleRadius + arrowLength), 0f), Quaternion.AngleAxis(-hakaAngle, Vector3.forward) * Vector3.left * hakaMultiplier);
+        Gizmos.DrawRay(new Vector3((circleRadius + arrowLength), 0f), Quaternion.AngleAxis(hakaAngle, Vector3.forward) * Vector3.left * hakaMultiplier);
+
+        // LEFT
+        Gizmos.DrawRay(new Vector3(-circleRadius, 0f), Vector3.left * arrowLength);
+        Gizmos.DrawRay(new Vector3(-(circleRadius + arrowLength), 0f), Quaternion.AngleAxis(-hakaAngle, Vector3.forward) * Vector3.right * hakaMultiplier);
+        Gizmos.DrawRay(new Vector3(-(circleRadius + arrowLength), 0f), Quaternion.AngleAxis(hakaAngle, Vector3.forward) * Vector3.right * hakaMultiplier);
+
+    }
+
+    #endregion
+}
