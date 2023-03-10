ab66cdd30bc2677c11f5c433e4a9097fdac840c3
MiksuMaker <91052332+MiksuMaker@users.noreply.github.com>
2023-03-10 13:43:47 +0200

    BezierPoints and Paths

--- Assets/_Code/Bezier/BezierPath.cs
+++ Assets/_Code/Bezier/BezierPath.cs
@@ -1,93 +1,103 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class BezierPath : MonoBehaviour
 {
     #region Properties
-    public GameObject A;
-    public GameObject B;
-    public GameObject C;
-    public GameObject D;
-    [Space(20)]
-    [Range(0,1)]
-    [SerializeField] float t = 0f;
-    [Space(20)]
-    [SerializeField] float indicatorRadius = 0.1f;
+    [SerializeField]
+    List<BezierPoint> points = new List<BezierPoint>();
+    #endregion
+
+
+    #region
     #endregion
 
     #region Gizmos
     private void OnDrawGizmos()
     {
-        if (A == null || B == null || C == null || D == null) { return; }
-
-        #region Setup
-        Vector3 a = A.transform.position;
-        Vector3 b = B.transform.position;
-        Vector3 c = C.transform.position;
-        Vector3 d = D.transform.position;
-
-        // Draw lines between the Points
-        DrawLine(a, b, Color.cyan);
-        DrawLine(b, c);
-        DrawLine(c, d);
-        #endregion
-
-        // Lerp
-        Vector3 PtX = (1 - t) * a + t * b;
-        Vector3 PtY = (1 - t) * b + t * c;
-        Vector3 PtZ = (1 - t) * c + t * d;
-
-        float rad = indicatorRadius;
-        Gizmos.DrawSphere(PtX, rad);
-        Gizmos.DrawSphere(PtY, rad);
-        Gizmos.DrawSphere(PtZ, rad);
-
-        // Draw lines between the Pt- points
-        DrawLine(PtX, PtY, Color.white);
-        DrawLine(PtY, PtZ);
-
-        // Lerp between the according points
-        Vector3 r_point = (1 - t) * PtX + t * PtY;
-        Vector3 s_point = (1 - t) * PtY + t * PtZ;
-
-        DrawLine(r_point, s_point, Color.magenta);
-        Gizmos.DrawSphere(r_point, rad);
-        Gizmos.DrawSphere(s_point, rad);
-
-        // Final Lerp for the BEZIER CURVE POINT
-        Vector3 bezier_point = (1 - t) * r_point + t * s_point;
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(bezier_point, rad * 2);
+        CalculatePath();
     }
     #endregion
 
-    #region BEZIER CALCULATION
+    #region Functions
+    [ContextMenu("Get All Points")]
+    private void GetAllPoints()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            // Add all children to the list
+            points.Add(transform.GetChild(i).GetComponent<BezierPoint>());
+        }
+    }
 
+    //[ContextMenu("Delete Most Points")]
+    private void DeleteAllPoints()
+    {
+        for (int i = transform.childCount; i > 0; i--)
+        {
+            // If only child, save them
+            if (i == 0) { break; }
+
+            // Destroy rest of the Children
+            GameObject child = transform.GetChild(i - 1).gameObject;
+            DestroyImmediate(child);
+        }
+
+        // Clear list
+        points.Clear();
+
+        GetAllPoints();
+    }
+
+    private void CalculatePath()
+    {
+
+        // Check points amount
+        bool drawLoop = true;
+        int pointCount = points.Count;
+        if (pointCount < 3) { drawLoop = false; }
+
+        // Draw Bezier Path
+        for (int i = 0; i < pointCount; i++)
+        {
+            Handles.DrawBezier(points[i].transform.position,
+                               //points[i + 1].transform.position,
+                               points[(i + 1) % pointCount].transform.position,
+                               points[i].control1.position,
+                               //points[i + 1].control0.position,
+                               points[(i + 1) % pointCount].control0.position,
+                               Color.white, default, 2f
+                               );
+
+            // Check if last of the line
+        }
+    }
     #endregion
+}
 
-    #region Helpers
-    private void DrawRay(Vector3 pos, Vector3 dir, Color c)
+static class Helpers
+{
+    static void DrawRay(Vector3 pos, Vector3 dir, Color c)
     {
         Gizmos.color = c;
         Gizmos.DrawRay(pos, dir);
     }
 
-    private void DrawLine(Vector3 pos, Vector3 pos2, Color c)
+    static void DrawLine(Vector3 pos, Vector3 pos2, Color c)
     {
         Gizmos.color = c;
         Gizmos.DrawLine(pos, pos2);
     }
 
-    private void DrawLine(Vector3 pos, Vector3 pos2)
+    static void DrawLine(Vector3 pos, Vector3 pos2)
     {
         Gizmos.DrawLine(pos, pos2);
     }
 
-    private void DebugDrawRay(Vector3 pos, Vector3 dir, Color c)
+    static void DebugDrawRay(Vector3 pos, Vector3 dir, Color c)
     {
         Debug.DrawRay(pos, dir, c, 3f);
     }
-    #endregion
 }
---
+++ Assets/_Code/Bezier/BezierPoint.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BezierPoint : MonoBehaviour
+{
+    #region Properties
+    public Transform control0;
+    public Transform control1;
+
+    private Vector3 anchor;
+    #endregion
+
+    #region Setup
+
+    #endregion
+
+    #region GIzmos
+    private void OnDrawGizmos()
+    {
+        anchor = transform.position;
+        Vector3 c0 = control0.position;
+        Vector3 c1 = control1.position;
+
+        // Draw Lines for ControlPoints
+        DrawLine(c0, anchor, Color.green);
+        DrawLine(c1, anchor, Color.red);
+
+        Handles.DrawBezier(c0, c1, anchor, anchor, Color.blue, Texture2D.whiteTexture, 1f);
+    }
+    #endregion
+
+    #region Helpers
+    private void DrawRay(Vector3 pos, Vector3 dir, Color c)
+    {
+        Gizmos.color = c;
+        Gizmos.DrawRay(pos, dir);
+    }
+
+    private void DrawLine(Vector3 pos, Vector3 pos2, Color c)
+    {
+        Gizmos.color = c;
+        Gizmos.DrawLine(pos, pos2);
+    }
+
+    private void DrawLine(Vector3 pos, Vector3 pos2)
+    {
+        Gizmos.DrawLine(pos, pos2);
+    }
+
+    private void DebugDrawRay(Vector3 pos, Vector3 dir, Color c)
+    {
+        Debug.DrawRay(pos, dir, c, 3f);
+    }
+    #endregion
+}
